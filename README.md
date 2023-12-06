3d26bf0cd0a39abb6d154bb006392b01e4b5cdd6
jibranabsarulislam <jga26@njit.edu>
2023-12-06 17:37:30 -0500

    init

---
+++ README.md
@@ -0,0 +1,20 @@
+# Installation Guide
+
+Please see https://github.com/notnotnotswipez/MoreCompany for the MoreCompany mod.
+
+## Description
+This is a simple script to install BepInEx and the MoreCompany mod for Lethal Company. Benefits include:
+- Automatic installation of BepInEx from the official repository
+- Automatic installation of the MoreCompany mod from Thunderstore
+    - TODO: Download and build from source
+
+## Prerequisites
+- Python 3.6 or higher
+
+## Installation
+- Clone this repository
+- Run `python install.py` in the root directory of this repository
+- Optional: Run `python install.py debug` to see debug messages
+
+## Notes
+- Windows systems should almost always use option 3 (BepInEx_x86)
---
+++ install.py
@@ -0,0 +1,132 @@
+import os
+import json
+import sys
+import shutil
+import zipfile
+import ssl
+import urllib.request
+
+
+class Utils:
+    def cleanup(filename):
+        print("Cleaning up " + filename)
+        if os.path.exists(filename):
+            os.remove(filename)
+
+    def extract(filename):
+        print("Extracting " + filename)
+        with zipfile.ZipFile(filename, "r") as zip_ref:
+            zip_ref.extractall()
+
+
+debug = False
+
+
+def print(*args, **kwargs):
+    force = kwargs.pop("force", False)
+    if debug or force:
+        __builtins__.print(*args, **kwargs)
+
+
+default_steam_dir = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Lethal Company"
+
+
+def install_bepinex_latest():
+    # Get latest release options from BePinEx repo
+    response = urllib.request.urlopen(
+        "https://api.github.com/repos/BepInEx/BepInEx/releases/latest"
+    )
+    data = response.read()
+    encoding = response.info().get_content_charset("utf-8")
+    result = json.loads(data.decode(encoding))
+    # version = result['tag_name']
+
+    # Select version to download
+
+    options = result["assets"]
+    # TODO: Check os and select correct platform
+    print("Select your platform:")
+    for i in range(1, len(options) + 1):
+        print("[" + str(i) + "] " + options[i - 1]["name"])
+
+    platform = int(input("Enter the number of your platform: "))
+    option = options[platform - 1]
+    filename = option["name"]
+    url = option["browser_download_url"]
+
+    # Download .zip and extract to script directory
+    print("Downloading " + url)
+    urllib.request.urlretrieve(url, filename)
+
+    Utils.extract(filename)
+    Utils.cleanup(filename)
+
+    core_filepath = "\\BepInEx\\core"
+
+    # Update BepInEx core
+    if os.path.exists(default_steam_dir):
+        print("Moving BepInEx to " + default_steam_dir)
+        if os.path.exists(default_steam_dir + core_filepath):
+            print("Removing old BepInEx core")
+            shutil.rmtree(default_steam_dir + core_filepath)
+        shutil.move(core_filepath[1:], default_steam_dir + "\\BepInEx\\")
+    else:
+        print(
+            "Could not find Steam directory, please move BepInEx to your game directory manually"
+        )
+
+
+def install_more_company_1_6_0():
+    filename = "MoreCompany.zip"
+
+    # Disable SSL verification to download from Thunderstore
+    # TODO: ...don't?
+    ssl._create_default_https_context = ssl._create_unverified_context
+    more_company_url = (
+        "https://thunderstore.io/package/download/notnotnotswipez/MoreCompany/1.6.0/"
+    )
+
+    print("Downloading " + more_company_url)
+    req = urllib.request.Request(
+        more_company_url, headers={"User-Agent": "Mozilla/5.0"}
+    )
+    response = urllib.request.urlopen(req)
+
+    with open(filename, "wb") as out_file:
+        data = response.read()
+        out_file.write(data)
+
+    Utils.extract(filename)
+    Utils.cleanup(filename)
+
+    # Move MoreCompany.dll to plugins folder
+    downloaded_dll_path = "\\BepInEx\\plugins\\MoreCompany.dll"
+    target_dll_path = default_steam_dir + downloaded_dll_path
+    shutil.copy(downloaded_dll_path[1:], target_dll_path)
+
+
+def post_install():
+    # Remove all files except repo files
+    exclusion_list = {"install.py", "README.md", ".git", ".gitignore", ".vscode"}
+    for filename in os.listdir("."):
+        if filename not in exclusion_list:
+            try:
+                os.remove(filename)
+            except:
+                shutil.rmtree(filename)
+            finally:
+                print("Removed " + filename)
+    print("Done!", force=True)
+
+
+def install():
+    install_bepinex_latest()
+    install_more_company_1_6_0()
+    post_install()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "debug":
+            debug = True
+    install()
